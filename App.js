9d65c5145d0ccb99405fec7ffdc97f0136b3225f
Mateus Soares <mateussoaress1997@gmail.com>
2021-09-08 21:16:56 -0300

    Hello world

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>A base React Native App!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
