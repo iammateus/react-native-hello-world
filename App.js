1c4b2e1ff4fcb3c217d118efa16f0d20dec57ab0
Mateus Soares <mateussoaress1997@gmail.com>
2021-09-08 21:05:02 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>A base React Native App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#CCC',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:12
+
+ENV REACT_NATIVE_PACKAGER_HOSTNAME=0.0.0.0
+ENV EXPO_DEVTOOLS_LISTEN_ADDRESS=0.0.0.0
+
+WORKDIR /app
+
+RUN apt-get update -y
+RUN yarn global add expo-cli
+ 
+COPY . .
+
+RUN yarn install
+
+EXPOSE 19000
+EXPOSE 19001
+EXPOSE 19002
+
+CMD yarn start
